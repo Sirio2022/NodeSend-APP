f820ae9efa77a2142ad928eea936bb443d478f1a
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-08 13:38:36 -0500

    Nice

--- controllers/archivosController.js
+++ controllers/archivosController.js
@@ -1,22 +1,25 @@
 const multer = require('multer');
 const shortid = require('shortid');
 
-const configuracionMulter = {
-  limits: { fileSize: 5000000 },
-  storage: (fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, __dirname + '/../uploads/');
-    },
-    filename: (req, file, cb) => {
-      const fileExtension = file.mimetype.split('/')[1];
-      cb(null, `${shortid.generate()}.${fileExtension}`);
-    },
-  })),
-};
-
-const upload = multer(configuracionMulter).single('archivo');
-
 exports.subirArchivo = async (req, res, next) => {
+  const configuracionMulter = {
+    limits: { fileSize: req.usuario ? 100000000 : 2000000 },
+    storage: (fileStorage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, __dirname + '/../uploads/');
+      },
+      filename: (req, file, cb) => {
+        const fileExtension = file.originalname.substring(
+          file.originalname.lastIndexOf('.'),
+          file.originalname.length
+        );
+        cb(null, `${shortid.generate()}${fileExtension}`);
+      },
+    })),
+  };
+
+  const upload = multer(configuracionMulter).single('archivo');
+
   upload(req, res, async function (error) {
     console.log(req.file);
     if (!error) {
