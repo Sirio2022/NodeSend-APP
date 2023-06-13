4c62ff451a4c5b6335ff54bd67a94df585915710
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-13 12:01:42 -0500

    Nice el cors

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const conectarDB = require('./config/db');
-
+const cors = require('cors');
 
 // Create a new express application instance
 const app = express();
 
-
-
 // Conectar a la base de datos
 conectarDB();
+
+// Habilitar Cors
+const opcionesCors = {
+  origin: process.env.FRONTEND_URL,
+};
+app.use(cors(opcionesCors));
 
 // Puerto de la app
 const port = process.env.PORT || 4000;
@@ -19,8 +23,8 @@ app.use(express.json());
 // Rutas de la app
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/enlaces', require('./routes/enlaces'));  
-app.use('/api/archivos', require('./routes/archivos'));  
+app.use('/api/enlaces', require('./routes/enlaces'));
+app.use('/api/archivos', require('./routes/archivos'));
 
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
